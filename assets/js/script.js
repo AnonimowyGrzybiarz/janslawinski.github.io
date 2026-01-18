727859774f6fdc0ac67a67ce8e28377b1422a19b
AnonimowyGrzybiarz <AnonimowyGrzybiarz@gmail.com>
2026-01-18 20:41:39 +0100

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,47 @@
-$( document ).ready(function() {
-                
-                $("#about_scroll").fadeOut();   
-                $("#work_scroll").fadeOut();
-                $("#contact_scroll").fadeOut();
+function scrollToTop() {
+    $('html, body').stop().animate({ scrollTop: 0 }, 300);
+}
 
-                $("#about").click(function(){
-                    $("#index").fadeOut();
-                    $("#about_scroll").fadeIn();
-                    $('#about_left').addClass('animated slideInLeft');
-                    $('#about_right').addClass('animated slideInRight');
-                    });
-                $("#work").click(function(){
-                    $("#index").fadeOut();
-                    $("#work_scroll").fadeIn();
-                    $('#work_left').addClass('animated slideInLeft');
-                    $('#work_right').addClass('animated slideInRight');
-                    });
-                $("#contact").click(function(){
-                    $("#index").fadeOut();
-                    $("#contact_scroll").fadeIn();
-                    $('#contact_left').addClass('animated slideInLeft');
-                    $('#contact_right').addClass('animated slideInRight');
-                    });
-                
-                $(".back").click(function(){
-                    $(".pages").fadeOut();
-                    $("#index").fadeIn();
-                    $('#index_left').addClass('animated slideInLeft');
-                    $('#index_right').addClass('animated slideInRight');
-                    });
-           
-		});
+$(document).ready(function () {
+
+    $("#about_scroll").fadeOut();
+    $("#work_scroll").fadeOut();
+    $("#contact_scroll").fadeOut();
+
+    $("#about").click(function () {
+        scrollToTop();
+        $("#index").fadeOut(function () {
+            $("#about_scroll").fadeIn();
+        });
+        $('#about_left').addClass('animated slideInLeft');
+        $('#about_right').addClass('animated slideInRight');
+    });
+
+    $("#work").click(function () {
+        scrollToTop();
+        $("#index").fadeOut(function () {
+            $("#work_scroll").fadeIn();
+        });
+        $('#work_left').addClass('animated slideInLeft');
+        $('#work_right').addClass('animated slideInRight');
+    });
+
+    $("#contact").click(function () {
+        scrollToTop();
+        $("#index").fadeOut(function () {
+            $("#contact_scroll").fadeIn();
+        });
+        $('#contact_left').addClass('animated slideInLeft');
+        $('#contact_right').addClass('animated slideInRight');
+    });
+
+    $(".back").click(function () {
+        scrollToTop();
+        $(".pages").fadeOut(function () {
+            $("#index").fadeIn();
+        });
+        $('#index_left').addClass('animated slideInLeft');
+        $('#index_right').addClass('animated slideInRight');
+    });
+
+});
